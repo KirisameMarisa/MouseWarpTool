426707e9f5a3cf1a2366c9a441defe53ec291948
kirisamemarisa <marisa.love0925@gmail.com>
2017-09-30 20:41:06 +0900

    fix * Added reset process of stopwatch

--- MouseWarpTool/Views/Behavior/MouseClickBeavior.cs
+++ MouseWarpTool/Views/Behavior/MouseClickBeavior.cs
@@ -16,6 +16,8 @@ namespace MouseWarpTool.Views.Behavior
     class MouseClickBeavior : Behavior<FrameworkElement>
     {
         private IKeyboardMouseEvents _globalHook = Hook.GlobalEvents();
+        private System.Diagnostics.Stopwatch _stopWatch = new System.Diagnostics.Stopwatch();
+        private DispatcherTimer _timer = new DispatcherTimer(DispatcherPriority.Normal);
 
         /// <summary>
         /// マウスクリックした時に実行されるコマンド
@@ -48,6 +50,9 @@ namespace MouseWarpTool.Views.Behavior
             {
                 return;
             }
+
+            _timer.Stop();
+            _stopWatch.Reset();
         }
 
         //!< 1s = 1000ms
@@ -63,16 +68,15 @@ namespace MouseWarpTool.Views.Behavior
 
             var designatedTime = TimeSpan.FromSeconds(config.IgnitionTime).TotalMilliseconds;
             //ストップウォッチを開始する
-            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
-            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Send);
-            timer.Interval = TimeSpan.FromMilliseconds(1);
-            timer.Start();                       
-            timer.Tick += (s, t) =>
+            _stopWatch.Start();
+            _timer.Interval = TimeSpan.FromMilliseconds(1);
+            _timer.Start();                       
+            _timer.Tick += (s, t) =>
             {
-                if (sw.Elapsed.TotalMilliseconds >= designatedTime)
+                if (_stopWatch.Elapsed.TotalMilliseconds >= designatedTime)
                 {
-                    sw.Stop();
-                    timer.Stop();
+                    _stopWatch.Reset();
+                    _timer.Stop();
                     if (Command != null && Command.CanExecute(sender))
                     {
                         Command.Execute(args);
